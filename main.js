3f3ff17fe32939c658a6b826ca65930bfb7ed5a6
Pontes <hpo.pontes@gmail.com>
2023-06-12 14:02:45 -0300

    puxando API

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log('Hello world')
+//console.log('Hello world')
+//O significado de API é Interface de Programação de Aplicações. Ela permite que dois componentes de software se comuniquem. Chamamos esses dois lados de cliente e servidor.
+var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/');
+console.log(consultaCep)
