6c94cc1eec1832152439131e53e09dc4145c4572
Pontes <hpo.pontes@gmail.com>
2023-06-12 14:43:32 -0300

    tratando erro com cath

--- main.js
+++ main.js
@@ -2,5 +2,9 @@
 //O significado de API é Interface de Programação de Aplicações. Ela permite que dois componentes de software se comuniquem. Chamamos esses dois lados de cliente e servidor.
 var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
 .then(resposta => resposta.json())
-.then(r => console.log(r));
+.then(r => {
+    if (r.erro){
+        throw Error('Cep não existe')
+    }else
+        console.log(r)});
 console.log(consultaCep)
