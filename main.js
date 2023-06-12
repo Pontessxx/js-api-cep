af096ba751f6a2ca9ade94f96f301e88e53c5fe8
Pontes <hpo.pontes@gmail.com>
2023-06-12 14:38:03 -0300

    Then e JSON

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 //console.log('Hello world')
 //O significado de API é Interface de Programação de Aplicações. Ela permite que dois componentes de software se comuniquem. Chamamos esses dois lados de cliente e servidor.
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/');
+var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+.then(resposta => resposta.json())
+.then(r => console.log(r));
 console.log(consultaCep)
