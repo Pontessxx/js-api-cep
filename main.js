c952a8fbf2c25a146d62db121a28d4cfe662b5e4
Pontes <hpo.pontes@gmail.com>
2023-06-12 14:50:26 -0300

    async e await

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 //console.log('Hello world')
 //O significado de API é Interface de Programação de Aplicações. Ela permite que dois componentes de software se comuniquem. Chamamos esses dois lados de cliente e servidor.
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if (r.erro){
-        throw Error('Cep não existe')
-    }else
-        console.log(r)})
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluido!'));
-console.log(consultaCep)
+async function buscaEndereco(){
+    var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/');
+    console.log(consultaCep);
+}
+console.log(buscaEndereco());
