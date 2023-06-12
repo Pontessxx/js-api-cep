8116da0404624275c8f1e4ad9f5f12010069c98a
Pontes <hpo.pontes@gmail.com>
2023-06-12 14:56:10 -0300

    try e throw

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 //console.log('Hello world')
 //O significado de API é Interface de Programação de Aplicações. Ela permite que dois componentes de software se comuniquem. Chamamos esses dois lados de cliente e servidor.
 async function buscaEndereco(){
+    try {
     var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/');
-    console.log(consultaCep);
+    var consultaCepConvertida = await consultaCep.json();
+    if (consultaCepConvertida.erro){
+        throw Error('Cep não existente!')
+    }
+    console.log(consultaCepConvertida);
+    }catch (erro){
+        console.log(erro)
+    }
 }
 console.log(buscaEndereco());
