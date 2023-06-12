8d909ac79037a5d11bac6a810dc7ba149c3fcf44
Pontes <hpo.pontes@gmail.com>
2023-06-12 15:03:21 -0300

    Promise all

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 //console.log('Hello world')
 //O significado de API é Interface de Programação de Aplicações. Ela permite que dois componentes de software se comuniquem. Chamamos esses dois lados de cliente e servidor.
-async function buscaEndereco(){
+async function buscaEndereco(cep){
     try {
-    var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/');
+    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     var consultaCepConvertida = await consultaCep.json();
-    if (consultaCepConvertida.erro){
+        if (consultaCepConvertida.erro){
         throw Error('Cep não existente!')
-    }
-    console.log(consultaCepConvertida);
+        }
+        console.log(consultaCepConvertida);
+        return consultaCepConvertida
     }catch (erro){
         console.log(erro)
     }
 }
-console.log(buscaEndereco());
+let ceps =  ['01001000','01001001']
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
